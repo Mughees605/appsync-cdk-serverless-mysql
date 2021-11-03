4e938bc175ac9a8fe98171f6b69a74e2ece0bf9d
mughees605 <muhammadmughees08@gmail.com>
2021-11-03 20:02:06 +0500

    removed config from gitignore

---
+++ configs/config.ts
@@ -0,0 +1,17 @@
+const config = {
+  account: '',
+  region: 'us-east-1',
+  DB_NAME: '',
+  DB_HOST: "",
+  DB_PASSWORD: "",
+  DB_PORT: "",
+  DB_USER: "",
+  NODE_ENV: '',
+  DB_SECRET:"",
+  DB_CLUSTER_ID:"",
+  vpcId: '',
+  availabilityZones: ['',  ''],
+  publicSubnetIds: ['', ''],
+}
+
+export { config }
